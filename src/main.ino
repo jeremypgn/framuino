1995805410cebccb2acaba78b807db89810af9f3
jeremypgn <pingeonjeremy@gmail.com>
2016-04-23 23:16:56 +0200

    Debug serial added

--- src/main.ino
+++ src/main.ino
@@ -1,9 +1,16 @@
+#include <SoftwareSerial.h>
+SoftwareSerial debug (12, 13);
+
+#include "func/wait.ino"
+
 void setup(){
+
   Serial.begin(9600);
+  debug.begin(9600);
 
 }
 
 void loop() {
-  wait();
-  delay(1000);
+  bool wait_return = wait("sec", 1);
+  Serial.println(wait_return);
 }
